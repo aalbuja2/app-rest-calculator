a71df8cad57fc4aab85012dec68eac6293286d81
Andrea Albuja <andrea.albuja93@gmail.com>
2023-02-12 15:27:23 -0500

    Add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('App Calculator')
+    .setDescription('This Api has the basic math operations like addition, subtraction, multiplication and division ')
+    .setVersion('1.0')
+    .addTag('calculator')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
