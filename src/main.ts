28b98ec23dbadc0f8d06096d1ac05a29fee40732
Andrea Albuja <andrea.albuja93@gmail.com>
2023-02-11 19:05:53 -0500

    Fix dependences

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
-  console.log(process.env.TYPEORM_HOST);
-  console.log(process.env.TYPEORM_DATABASE);
-  console.log(process.env.TYPEORM_USERNAME);
-  console.log(process.env.TYPEORM_PASSWORD);
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.SERVER_PORT);
 }
